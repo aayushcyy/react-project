cb92bb5a34134dbbcd421f78143e8c2519e37859
Aayush Chaudhary <aayushcyy@gmail.com>
2024-10-15 18:22:58 +0530

    add passGenerator folder

---
+++ passGenerator/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>Hello this is the second time of password generator</p>
+    </>
+  );
+}
+
+export default App;
